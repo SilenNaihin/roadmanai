05a2598a1cf533a5d69ed7d7c9b1912e4d18a353
Silen Naihin <silen.naihin@gmail.com>
2023-05-29 18:10:46 -0400

    init

---
+++ app/api/transcribe.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const res = await fetch('', {
+    headers: {
+      'Content-Type': 'application/json',
+      'API-Key': process.env.DATA_API_KEY,
+    },
+  });
+  const data = await res.json();
+
+  return NextResponse.json({ data });
+}
