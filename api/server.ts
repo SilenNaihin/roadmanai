0152e7bc4a9c3f7df76ca7d373b86f7dfc5d37c7
Silen Naihin <silen.naihin@gmail.com>
2023-06-03 16:24:25 -0400

    updating server based on vercel example

--- api/server.ts
+++ api/server.ts
@@ -4,106 +4,123 @@ import cors from 'cors';
 import { exec } from 'child_process';
 import path from 'path';
 import fs from 'fs';
+import next from 'next';
+import { parse } from 'url';
+import { NextServer, RequestHandler } from 'next/dist/server/next';
 
-const app = express();
+const port = parseInt(process.env.PORT || '3000', 10);
+const dev: boolean = process.env.NODE_ENV !== 'production';
+console.log('NODE_ENV:', process.env.NODE_ENV);
+const nextApp: NextServer = next({ dev });
 
-// Create upload directory if it doesn't exist
-const uploadDir = path.resolve(__dirname, '../public/uploads');
+console.log(nextApp);
 
-// Multer setup
-const storage = diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadDir);
-  },
-  filename: function (req, file, cb) {
-    cb(null, `tmp-audio.webm`);
-  },
-});
+const nextHandler: RequestHandler = nextApp.getRequestHandler();
 
-const upload = multer({ storage });
+nextApp.prepare().then(async () => {
+  const app = express();
 
-interface MulterRequest extends Request {
-  file?: Express.Multer.File;
-}
+  interface MulterRequest extends Request {
+    file?: Express.Multer.File;
+  }
 
-app.use(cors());
-app.use(express.json());
+  app.all('*', (req: Request, res: Response) => {
+    const parsedUrl = parse(req.url, true);
+    return nextHandler(req, res, parsedUrl);
+  });
 
-app.post(
-  '/transcribe',
-  upload.single('audioFile'),
-  (req: MulterRequest, res: Response) => {
-    const filePath = req.file?.path;
+  // Create upload directory if it doesn't exist
+  const uploadDir = path.resolve(__dirname, '../public/uploads');
 
-    let sCommand = `python -m api.whisper --filePath "${filePath}"`;
+  // Multer setup
+  const storage = diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, uploadDir);
+    },
+    filename: function (req, file, cb) {
+      cb(null, `tmp-audio.webm`);
+    },
+  });
+
+  const upload = multer({ storage });
+
+  app.use(cors());
+  app.use(express.json());
+
+  app.post(
+    '/transcribe',
+    upload.single('audioFile'),
+    (req: MulterRequest, res: Response) => {
+      const filePath = req.file?.path;
+
+      let sCommand = `python -m api.whisper --filePath "${filePath}"`;
+
+      exec(sCommand, (err, stdout, stderr) => {
+        if (err) {
+          res.send({ status: 300, error: err.message, out: null, file: null });
+        } else {
+          const json = JSON.parse(stdout);
+          const transcript = json.text;
+          res.send({
+            status: 200,
+            error: stderr,
+            out: stdout,
+            file: req.file,
+            transcript,
+          });
+
+          // Delete the file
+          fs.unlink(`${uploadDir}/tmp-audio.webm`, (err) => {
+            if (err) {
+              console.error('Could not delete file: ', err);
+            }
+          });
+        }
+      });
+    }
+  );
+
+  app.post('/completions', (req: Request, res: Response) => {
+    let sCommand = `python -m api.completions --text "${req.body.transcript}" --type "${req.body.type}"`;
 
     exec(sCommand, (err, stdout, stderr) => {
       if (err) {
         res.send({ status: 300, error: err.message, out: null, file: null });
       } else {
-        const json = JSON.parse(stdout);
-        const transcript = json.text;
+        const json: { translate: string; phonetic: string } =
+          JSON.parse(stdout);
         res.send({
           status: 200,
           error: stderr,
           out: stdout,
           file: req.file,
-          transcript,
-        });
-
-        // Delete the file
-        fs.unlink(`${uploadDir}/tmp-audio.webm`, (err) => {
-          if (err) {
-            console.error('Could not delete file: ', err);
-          }
+          translation: json.translate,
+          phonetic: json.phonetic,
         });
       }
     });
-  }
-);
-
-app.post('/completions', (req: Request, res: Response) => {
-  let sCommand = `python -m api.completions --text "${req.body.transcript}" --type "${req.body.type}"`;
-
-  exec(sCommand, (err, stdout, stderr) => {
-    if (err) {
-      res.send({ status: 300, error: err.message, out: null, file: null });
-    } else {
-      const json: { translate: string; phonetic: string } = JSON.parse(stdout);
-      res.send({
-        status: 200,
-        error: stderr,
-        out: stdout,
-        file: req.file,
-        translation: json.translate,
-        phonetic: json.phonetic,
-      });
-    }
   });
-});
 
-let { env } = process;
-env.PATH += ';C:\\Program Files\\ffmpeg\\bin';
+  let { env } = process;
+  env.PATH += ';C:\\Program Files\\ffmpeg\\bin';
 
-app.post('/eleven', (req: Request, res: Response) => {
-  let sCommand = `python -m api.eleven --text "${req.body.speech}"`;
+  app.post('/eleven', (req: Request, res: Response) => {
+    let sCommand = `python -m api.eleven --text "${req.body.speech}"`;
 
-  exec(sCommand, { env: env }, (err, stdout, stderr) => {
-    if (err) {
-      res.send({ status: 300, error: err.message, out: null, file: null });
-    } else {
-      res.send({
-        status: 200,
-        error: stderr,
-        out: stdout,
-        file: req.file,
-        responseAudio: stdout,
-      });
-    }
+    exec(sCommand, { env: env }, (err, stdout, stderr) => {
+      if (err) {
+        res.send({ status: 300, error: err.message, out: null, file: null });
+      } else {
+        res.send({
+          status: 200,
+          error: stderr,
+          out: stdout,
+          file: req.file,
+          responseAudio: stdout,
+        });
+      }
+    });
   });
+
+  app.listen(port, () => console.log(`Server listening on port ${port}`));
 });
-
-const port = 3001;
-app.listen(port, () => console.log(`Server listening on port ${port}`));
-
-module.exports = app;
