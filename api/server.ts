d5a0a1153cf0e37f4bc8c496f0ac811894fb8d16
Silen Naihin <silen.naihin@gmail.com>
2023-06-03 22:38:10 -0400

    basic server changes

--- api/server.ts
+++ api/server.ts
@@ -10,115 +10,127 @@ import { NextServer, RequestHandler } from 'next/dist/server/next';
 
 const port = parseInt(process.env.PORT || '3000', 10);
 const dev: boolean = process.env.NODE_ENV !== 'production';
-const nextApp: NextServer = next({ dev });
+const hostname = 'localhost';
+const nextApp: NextServer = next({ dev, hostname, port });
 
 const nextHandler: RequestHandler = nextApp.getRequestHandler();
 
-nextApp.prepare().then(async () => {
-  const app = express();
+nextApp
+  .prepare()
+  .then(async () => {
+    const app = express();
 
-  interface MulterRequest extends Request {
-    file?: Express.Multer.File;
-  }
+    interface MulterRequest extends Request {
+      file?: Express.Multer.File;
+    }
 
-  // Create upload directory if it doesn't exist
-  const uploadDir = path.resolve(__dirname, '../public/uploads');
+    // Create upload directory if it doesn't exist
+    const uploadDir = path.resolve(__dirname, '../public/uploads');
 
-  // Multer setup
-  const storage = diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, uploadDir);
-    },
-    filename: function (req, file, cb) {
-      cb(null, `tmp-audio.webm`);
-    },
-  });
+    // Multer setup
+    const storage = diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, uploadDir);
+      },
+      filename: function (req, file, cb) {
+        cb(null, `tmp-audio.webm`);
+      },
+    });
 
-  const upload = multer({ storage });
+    const upload = multer({ storage });
 
-  app.use(cors());
-  app.use(express.json());
+    app.use(cors());
+    app.use(express.json());
 
-  app.post(
-    '/transcribe',
-    upload.single('audioFile'),
-    (req: MulterRequest, res: Response) => {
-      const filePath = req.file?.path;
+    app.post(
+      '/transcribe',
+      upload.single('audioFile'),
+      (req: MulterRequest, res: Response) => {
+        const filePath = req.file?.path;
 
-      let sCommand = `python -m api.whisper --filePath "${filePath}"`;
+        let sCommand = `python -m api.whisper --filePath "${filePath}"`;
+
+        exec(sCommand, (err, stdout, stderr) => {
+          if (err) {
+            res.send({
+              status: 300,
+              error: err.message,
+              out: null,
+              file: null,
+            });
+          } else {
+            const json = JSON.parse(stdout);
+            const transcript = json.text;
+            res.send({
+              status: 200,
+              error: stderr,
+              out: stdout,
+              file: req.file,
+              transcript,
+            });
+
+            // Delete the file
+            fs.unlink(`${uploadDir}/tmp-audio.webm`, (err) => {
+              if (err) {
+                console.error('Could not delete file: ', err);
+              }
+            });
+          }
+        });
+      }
+    );
+
+    app.post('/completions', (req: Request, res: Response) => {
+      console.log('server response');
+      let sCommand = `python -m api.completions --text "${req.body.transcript}" --type "${req.body.type}"`;
 
       exec(sCommand, (err, stdout, stderr) => {
         if (err) {
           res.send({ status: 300, error: err.message, out: null, file: null });
         } else {
-          const json = JSON.parse(stdout);
-          const transcript = json.text;
+          const json: { translate: string; phonetic: string } =
+            JSON.parse(stdout);
           res.send({
             status: 200,
             error: stderr,
             out: stdout,
             file: req.file,
-            transcript,
-          });
-
-          // Delete the file
-          fs.unlink(`${uploadDir}/tmp-audio.webm`, (err) => {
-            if (err) {
-              console.error('Could not delete file: ', err);
-            }
+            translation: json.translate,
+            phonetic: json.phonetic,
           });
         }
       });
-    }
-  );
-
-  app.post('/completions', (req: Request, res: Response) => {
-    console.log('server response');
-    let sCommand = `python -m api.completions --text "${req.body.transcript}" --type "${req.body.type}"`;
-
-    exec(sCommand, (err, stdout, stderr) => {
-      if (err) {
-        res.send({ status: 300, error: err.message, out: null, file: null });
-      } else {
-        const json: { translate: string; phonetic: string } =
-          JSON.parse(stdout);
-        res.send({
-          status: 200,
-          error: stderr,
-          out: stdout,
-          file: req.file,
-          translation: json.translate,
-          phonetic: json.phonetic,
-        });
-      }
     });
-  });
 
-  let { env } = process;
-  env.PATH += ';C:\\Program Files\\ffmpeg\\bin';
+    let { env } = process;
+    env.PATH += ';C:\\Program Files\\ffmpeg\\bin';
 
-  app.post('/eleven', (req: Request, res: Response) => {
-    let sCommand = `python -m api.eleven --text "${req.body.speech}"`;
+    app.post('/eleven', (req: Request, res: Response) => {
+      let sCommand = `python -m api.eleven --text "${req.body.speech}"`;
 
-    exec(sCommand, { env: env }, (err, stdout, stderr) => {
-      if (err) {
-        res.send({ status: 300, error: err.message, out: null, file: null });
-      } else {
-        res.send({
-          status: 200,
-          error: stderr,
-          out: stdout,
-          file: req.file,
-          responseAudio: stdout,
-        });
-      }
+      exec(sCommand, { env: env }, (err, stdout, stderr) => {
+        if (err) {
+          res.send({ status: 300, error: err.message, out: null, file: null });
+        } else {
+          res.send({
+            status: 200,
+            error: stderr,
+            out: stdout,
+            file: req.file,
+            responseAudio: stdout,
+          });
+        }
+      });
     });
-  });
 
-  app.all('*', (req, res) => {
-    const parsedUrl = parse(req.url, true);
-    return nextHandler(req, res, parsedUrl);
-  });
+    app.all('*', (req, res) => {
+      const parsedUrl = parse(req.url, true);
+      return nextHandler(req, res, parsedUrl);
+    });
 
-  app.listen(port, () => console.log(`Server listening on port ${port}`));
-});
+    app.listen(port, () => console.log(`Server listening on port ${port}`));
+  })
+  .catch((err) => {
+    console.error('Error occurred starting server:', err);
+    process.exit(1);
+  });
