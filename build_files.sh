4c459b494000a54e5485bbf6838b28a23324c1f3
Silen Naihin <silen.naihin@gmail.com>
2023-06-05 11:00:32 -0400

    adding build.sh to build

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
-pip install -r requirements.txt
-# python3.9 manage.py collectstatic
+# python3.9 -m venv env  # Create a virtual environment
+# source env/bin/activate  # Activate the virtual environment
+mkdir -p staticfiles_build
+pip install -r requirements.txt  # Install requirements
