1c1d92f147e586c860f34289403ced46e4438e76
Silen Naihin <silen.naihin@gmail.com>
2023-06-05 11:06:41 -0400

    trying to run requirements on startup

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,3 @@
 # python3.9 -m venv env  # Create a virtual environment
 # source env/bin/activate  # Activate the virtual environment
-mkdir -p staticfiles_build
 pip install -r requirements.txt  # Install requirements
